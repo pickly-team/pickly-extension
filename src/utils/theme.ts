7840cde5865f099fff96d8d2302396c53a58faee
ww8007 <wshmin1234@gmail.com>
2023-09-18 20:29:24 +0900

    design: 기본 theme 추가

---
+++ src/utils/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+	colors: {
+		primary: "#76B100",
+		lightPrimary: "#A7E100",
+		darkPrimary: "#8EC520",
+		lightGreen: "#BBFFB5",
+		buttonGreen: "#10AB10",
+		white: "#ffffff",
+		black: "#161617",
+		grey200: "#EEEEEE",
+		grey400: "#BDBDBD",
+		grey600: "#757575",
+		grey700: "#616161",
+		grey800: "#424242",
+		grey850: "#303030",
+		grey900: "#212121",
+		darkBlack: "#161514",
+		darkGrey: "#2A2A2A",
+		red: "#D53C22",
+		lightYellow: "#FFF8B5"
+	}
+} as const;
+
+export type ColorType = keyof typeof theme.colors;
