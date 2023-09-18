a976cd21329ccd048370fe762fafde6145f83808
ww8007 <wshmin1234@gmail.com>
2023-09-19 07:25:58 +0900

    feat: 무한 스크롤에 이용 가능한 custom hook 추가

---
+++ src/hooks/useBottomIntersection.ts
@@ -0,0 +1,36 @@
+import { useCallback, useRef } from "react";
+import useIntersection from "./useIntersection";
+
+interface IntersectionObserverProps {
+	fetchNextPage: () => void;
+	rootElement?: HTMLElement | null;
+	enabled?: boolean;
+}
+
+const useBottomIntersection = ({
+	fetchNextPage,
+	rootElement = null,
+	enabled = true
+}: IntersectionObserverProps) => {
+	const bottom = useRef(null);
+
+	const onIntersect = useCallback(
+		([entry]: IntersectionObserverEntry[]) => {
+			if (!enabled) return;
+			if (entry.isIntersecting) {
+				fetchNextPage();
+			}
+		},
+		[fetchNextPage, enabled, rootElement]
+	);
+
+	useIntersection({
+		onIntersect,
+		target: bottom,
+		rootElement: rootElement
+	});
+
+	return { bottom };
+};
+
+export default useBottomIntersection;
