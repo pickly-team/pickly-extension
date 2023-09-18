565a4d1d10c74599bc6858a3430c7cb4d2e96ee6
ww8007 <wshmin1234@gmail.com>
2023-09-18 20:29:11 +0900

    feat: href 받아오는 통신 코드 추가

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,15 @@
-console.log('background script loaded');
+import createContextMenu from "./utils/contextMenu";
+
+chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
+	if (request.message === "GET_HREF") {
+		sendResponse({ received: true });
+		return true;
+	}
+	if (request.message === "READY") {
+		sendResponse({ received: true });
+		return true;
+	}
+	return true;
+});
+
+createContextMenu();
