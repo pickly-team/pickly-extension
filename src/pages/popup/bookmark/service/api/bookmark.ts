d2cd7f0342bab3720ca80b6e0d96f6aec1420d70
ww8007 <wshmin1234@gmail.com>
2023-11-12 04:56:00 +0900

    feat: 북마크 리스트 관련 컴포넌트 추가

---
+++ src/pages/popup/bookmark/service/api/bookmark.ts
@@ -0,0 +1,117 @@
+// 북마크 메인 페이지
+// 북마크 리스트 조회
+
+import client from "@src/utils/client";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { READ_OPTION, READ_OPTIONS } from "../hooks/useReadList";
+
+/** API call 결과 */
+export interface SeverBookMarkItem {
+	hasNext: boolean;
+	contents: BookmarkItemType[];
+}
+
+export interface BookmarkItemType {
+	bookmarkId: number;
+	title: string;
+	url: string;
+	previewImageUrl: string;
+	isUserLike: boolean;
+	readByUser: boolean;
+	commentCnt: number;
+	createdDate: string;
+	disabled?: boolean;
+	categoryName: string;
+	categoryEmoji: string;
+}
+
+/** mapping 결과 */
+export type bookmarkGETBookMarkList = BookmarkItemType[];
+
+interface GETBookMarkListRequest {
+	memberId: string;
+	categoryId: number | null;
+	readByUser: READ_OPTION;
+	pageRequest?: {
+		cursorId?: number;
+		pageSize?: number;
+	};
+}
+
+const GETBookMarkList = {
+	API: async (params: GETBookMarkListRequest) => {
+		const { data } = await client.get<SeverBookMarkItem>(
+			`/members/bookmarks/chrome-extension`,
+			{
+				params: {
+					memberId: params.memberId,
+					categoryId: params.categoryId === 0 ? null : params.categoryId,
+					readByUser: READ_OPTIONS[params.readByUser],
+					cursorId: params.pageRequest?.cursorId,
+					pageSize: params.pageRequest?.pageSize
+				}
+			}
+		);
+
+		return {
+			hasNext: data.hasNext,
+			contents: GETBookMarkList.Mapper(data.contents)
+		};
+	},
+	Mapper: (bookmarkList: BookmarkItemType[]): bookmarkGETBookMarkList => {
+		console.log(process.env.VITE_ASSETS_URL);
+		return bookmarkList.map((bookmark) => ({
+			bookmarkId: bookmark.bookmarkId,
+			title: bookmark.title,
+			url: bookmark.url,
+			previewImageUrl:
+				bookmark.previewImageUrl ??
+				import.meta.env.VITE_ASSETS_URL + "/main.webp",
+			isUserLike: bookmark.isUserLike,
+			readByUser: bookmark.readByUser,
+			commentCnt: bookmark.commentCnt,
+			createdDate: bookmark.createdDate,
+			categoryName: bookmark.categoryName,
+			categoryEmoji: bookmark.categoryEmoji
+		}));
+	}
+};
+
+export const GET_BOOKMARK_LIST = (params: GETBookMarkListRequest) => [
+	"GET_BOOKMARK_LIST",
+	params.memberId,
+	params.readByUser,
+	params.categoryId
+];
+
+export const useGETBookMarkListQuery = (params: GETBookMarkListRequest) => {
+	return useInfiniteQuery(
+		GET_BOOKMARK_LIST(params),
+		async ({ pageParam = null }) => {
+			const { contents, hasNext } = await GETBookMarkList.API({
+				...params,
+				pageRequest: {
+					cursorId: pageParam,
+					pageSize: 10
+				}
+			});
+			return {
+				contents,
+				hasNext
+			};
+		},
+		{
+			getNextPageParam: (lastPage) => {
+				if (lastPage.hasNext) {
+					return lastPage.contents[lastPage.contents.length - 1].bookmarkId;
+				}
+				return undefined;
+			},
+			refetchOnWindowFocus: false,
+			cacheTime: 5 * 60 * 1000,
+			staleTime: 5 * 60 * 1000,
+			enabled: params.memberId !== "",
+			suspense: true
+		}
+	);
+};
---
+++ src/pages/popup/bookmark/service/store/bookmark.ts
@@ -0,0 +1,80 @@
+import { create } from "zustand";
+import { READ_OPTION } from "../hooks/useReadList";
+
+export const MAX_CATEGORY_COUNT = 20 as const;
+
+interface OptionType {
+	value: string | null;
+	label: string;
+}
+
+interface BookmarkInfo {
+	title: string;
+	url: string;
+	thumbnail: string;
+}
+
+interface BookmarkStore {
+	categoryOptions: OptionType[];
+	setCategoryOptions: (options: OptionType[]) => void;
+	selectedCategoryId: number | null;
+	setSelectedCategoryId: (id: number | null) => void;
+	readOption: READ_OPTION;
+	setReadOption: (option: READ_OPTION) => void;
+
+	bookmarkInfo: BookmarkInfo;
+	setBookmarkInfo: (fn: (info: BookmarkInfo) => BookmarkInfo) => void;
+	initializeBookmarkInfo: () => void;
+	fromPath: string;
+	setFromPath: (path: string) => void;
+	selectedBookmarkId: number;
+	setSelectedBookmarkId: (id: number) => void;
+	isBookmarkError: boolean;
+	setIsBookmarkError: (isBookmarkError: boolean) => void;
+}
+
+const useBookmarkStore = create<BookmarkStore>((set) => ({
+	categoryOptions: [],
+	setCategoryOptions: (options) => {
+		set({ categoryOptions: options });
+	},
+	selectedCategoryId: null,
+	setSelectedCategoryId: (id) => {
+		set({ selectedCategoryId: id });
+	},
+	readOption: "📖 전체",
+	setReadOption: (option) => {
+		set({ readOption: option });
+	},
+	bookmarkInfo: {
+		title: "",
+		url: "",
+		thumbnail: ""
+	},
+	setBookmarkInfo: (fn) => {
+		set((state) => ({ bookmarkInfo: fn(state.bookmarkInfo) }));
+	},
+	initializeBookmarkInfo: () => {
+		set({
+			bookmarkInfo: {
+				title: "",
+				url: "",
+				thumbnail: ""
+			}
+		});
+	},
+	fromPath: "",
+	setFromPath: (path) => {
+		set({ fromPath: path });
+	},
+	selectedBookmarkId: 0,
+	setSelectedBookmarkId: (id) => {
+		set({ selectedBookmarkId: id });
+	},
+	isBookmarkError: false,
+	setIsBookmarkError: (isBookmarkError) => {
+		set({ isBookmarkError });
+	}
+}));
+
+export default useBookmarkStore;
